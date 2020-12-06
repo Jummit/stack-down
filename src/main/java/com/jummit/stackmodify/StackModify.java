8d62976cb3934c72bf564e4eef05640d234ff405
Jummit <jummit@web.de>
2020-12-06 19:42:52 +0100

    initial commit

---
+++ src/main/java/com/jummit/stackmodify/StackModify.java
@@ -0,0 +1,16 @@
+package com.jummit.stackmodify;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("stackmodify")
+public class StackModify
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public StackModify() {
+    }
+
+}
