f07eb4e5d15f66b72f0d97bce4804da42c02c12f
Jummit <jummit@web.de>
2020-12-18 14:27:37 +0100

    start using server config

--- src/main/java/com/jummit/stackdown/config/StackDownConfig.java
+++ src/main/java/com/jummit/stackdown/config/StackDownConfig.java
@@ -10,31 +10,31 @@ import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 
 public class StackDownConfig {
 	
-	public static final CommonConfig COMMON;
-	public static final ForgeConfigSpec COMMON_SPEC;
+	public static final ServerConfig SERVER;
+	public static final ForgeConfigSpec SERVER_SPEC;
 	static {
-		final Pair<CommonConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(CommonConfig::new);
-		COMMON_SPEC = specPair.getRight();
-		COMMON = specPair.getLeft();
+		final Pair<ServerConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
+		SERVER_SPEC = specPair.getRight();
+		SERVER = specPair.getLeft();
 	}
 	
-	public static class CommonConfig {
+	public static class ServerConfig {
 		
 		public final ConfigValue<String> itemStackSize;
 		public final ConfigValue<String> blockStackSize;
 		public final ConfigValue<List<? extends String>> stackSizes;
 		
-		CommonConfig(ForgeConfigSpec.Builder builder) {
+		ServerConfig(ForgeConfigSpec.Builder builder) {
 			itemStackSize = builder
-				.comment("The max stack size modifier of all non-block items.\nModifiers can be operators like +, -, * and /.\nExamples:\n	+3\n	*5\n	/6\n	3\n	15\nIf this is empty, the stack sizes will only be modified when specified in the stackSizes list.")
-				.define("itemStackSize", "");
+					.comment("The max stack size modifier of all non-block items.\nModifiers can be operators like +, -, * and /.\nExamples:\n	+3\n	*5\n	/6\n	3\n	15\nIf this is empty, the stack sizes will only be modified when specified in the stackSizes list.")
+					.define("itemStackSize", "");
 			blockStackSize = builder
-				.comment("The max stack size modifier of all block items.")
-				.define("blockStackSize", "");
+					.comment("The max stack size modifier of all block items.")
+					.define("blockStackSize", "");
 			stackSizes = builder
-				.comment("A list of item patterns and their max stack size modifiers.\nPatterns can be items, mods, regular expressions surrounded by brackets, max stack sizes or tags.\nModifiers can be operators like +, -, * and / or an equal sign.")
-			.comment("Examples:\n[\n	\"minecraft:stone=64\",\n	\"16=64\",\n	\"forge:stone/32\",\n	\"(diamond_.*)+30\",\n	\"forestry-30\"\n]")
-				.defineList("stackSizes", new ArrayList<String>(), s -> s instanceof String);
+					.comment("A list of item patterns and their max stack size modifiers.\nPatterns can be items, mods, regular expressions surrounded by brackets, max stack sizes or tags.\nModifiers can be operators like +, -, * and / or an equal sign.")
+					.comment("Examples:\n[\n	\"minecraft:stone=64\",\n	\"16=64\",\n	\"forge:stone/32\",\n	\"(diamond_.*)+30\",\n	\"forestry-30\"\n]")
+					.defineList("stackSizes", new ArrayList<String>(), s -> s instanceof String);
 		}
 	}
 }
